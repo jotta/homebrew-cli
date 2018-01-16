56fec61caa5459e2be625a796a6ec514457fe350
gitlab-ci <ops@jotta.no>
2018-01-16 14:56:05 +0000

    Update homebrew release jotta-cli_0.3.4195_darwin_amd64.zip with sha 05b7bedf1496367286cf6462f12525261a6a9eaedf0dc708009595439bdc04c2

--- jotta-cli.rb
+++ jotta-cli.rb
@@ -3,11 +3,11 @@ require 'formula'
 class JottaCli < Formula
   desc "Jottacloud CLI"
   homepage "https://www.jottacloud.com/"
-  version "0.3.4183"
+  version "0.3.4195"
 
   if Hardware::CPU.is_64_bit?
-    url "https://repo.jotta.us/archives/darwin/amd64/jotta-cli_0.3.4183_darwin_amd64.zip"
-    sha256 "d4075cc087e1999697bd4530986b9e49801bffa22a1e09d4273a53fa60548f99"
+    url "https://repo.jotta.us/archives/darwin/amd64/jotta-cli_0.3.4195_darwin_amd64.zip"
+    sha256 "05b7bedf1496367286cf6462f12525261a6a9eaedf0dc708009595439bdc04c2"
   end
 
   depends_on :arch => :intel
