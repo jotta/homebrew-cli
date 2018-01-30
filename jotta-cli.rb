14966a5bdc56fdd5a1a051fe97b57de016aed224
Roy Viggo Larsen <roy@existemi.no>
2018-01-30 10:44:32 +0100

    EOS.undent is deprecated

--- jotta-cli.rb
+++ jotta-cli.rb
@@ -18,7 +18,7 @@ class JottaCli < Formula
 
   plist_options :manual => "jottad"
 
-  def plist; <<-EOS.undent
+  def plist; <<-EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
