55c2f00cd9a4aba3f08bb3eac19d932ea1fc2957
rolf <rolf@jotta.no>
2020-10-08 13:50:13 +0200

    Adding unstable recipe

---
+++ jotta-cli-unstable.rb
@@ -0,0 +1,46 @@
+require 'formula'
+
+class JottaCliUnstable < Formula
+  desc "Jottacloud CLI"
+  homepage "https://www.jottacloud.com/"
+  version "0.6.32724"
+
+  if Hardware::CPU.is_64_bit?
+    url "https://repo.jotta.us/archives/darwin/amd64/jotta-cli_0.6.32724_darwin_amd64.zip"
+    sha256 "28e36a2841fe044620773fb1a00b0ee9419a8260d3671213f69dfe74b1cd7bb6"
+  end
+
+  conflicts_with "jotta-cli", :because => "jotta-cli is installed, brew uninstall jotta-cli before installing jotta-cli-unstable"
+
+  depends_on :arch => :intel
+
+  def install
+    bin.install 'jottad', 'jotta-cli'
+  end
+
+  plist_options :manual => "jottad"
+
+  def plist; <<-EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{bin}/jottad</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+    </plist>
+    EOS
+  end
+
+  test do
+    system bin/"jotta-cli", "version"
+  end
+end
