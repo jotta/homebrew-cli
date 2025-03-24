5f6eac949ac573077b64a5679257506a582319eb
gitlab-ci <ops@jotta.no>
2025-03-24 12:30:31 +0000

    Update jotta-cli-unstable.rb homebrew release jotta-cli_0.17.132485_darwin_amd64.zip/jotta-cli_0.17.132485_darwin_arm64.zip with sha 14386743284155a123147b68ee6c980593d5e2a6102ac0c1a3adff479fe11890/6e73d811857b68011346844a3ece60e5c158ad3b2e54c7a9530153a4eb46cb8f

--- jotta-cli-unstable.rb
+++ jotta-cli-unstable.rb
@@ -3,14 +3,14 @@ require 'formula'
 class JottaCliUnstable < Formula
   desc "Jottacloud CLI"
   homepage "https://www.jottacloud.com/"
-    version "0.16.129389"
+    version "0.17.132485"
 
   if Hardware::CPU.intel?
-      url "https://repo.jotta.cloud/archives-unstable/darwin/amd64/jotta-cli_0.16.129389_darwin_amd64.zip"
-      sha256 "3e075bd5731eccb6f07eabee820bb885d593e21d6601234a70cac31a63203cfb"
+      url "https://repo.jotta.cloud/archives-unstable/darwin/amd64/jotta-cli_0.17.132485_darwin_amd64.zip"
+      sha256 "14386743284155a123147b68ee6c980593d5e2a6102ac0c1a3adff479fe11890"
   elsif Hardware::CPU.arm?
-      url "https://repo.jotta.cloud/archives-unstable/darwin/arm64/jotta-cli_0.16.129389_darwin_arm64.zip"
-      sha256 "58227e38656e815ec2a51bd57a6f17469cfcdeba503bd498742bf5e1065456b4"
+      url "https://repo.jotta.cloud/archives-unstable/darwin/arm64/jotta-cli_0.17.132485_darwin_arm64.zip"
+      sha256 "6e73d811857b68011346844a3ece60e5c158ad3b2e54c7a9530153a4eb46cb8f"
   end
 
   conflicts_with "jotta-cli", :because => "jotta-cli is installed, brew uninstall jotta-cli before installing jotta-cli-unstable"
